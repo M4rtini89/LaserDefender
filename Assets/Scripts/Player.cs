5d970241f9c4e3bfa4c24da81d4eccf982289833
MartinVN <m4rtini89@gmail.com>
2018-09-23 16:08:13 +0200

    Lecture 85

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10; // Units per second
+    [SerializeField] private float positionClampX = 5;
+    [SerializeField] private float positionClampY = 9;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        var deltaY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, -positionClampX, positionClampX);
+        var newYPox = Mathf.Clamp(transform.position.y + deltaY, -positionClampY, positionClampY);
+        var newPosition = new Vector2(newXPos, newYPox);
+        transform.position = newPosition;
+    }
+}
